ba0951962465539c70119113052973619416a325
Elmira Dzhy <elmira.freshcode@gmail.com>
2023-09-05 15:42:48 +0300

    Add typecasting example

--- public/app.js
+++ public/app.js
@@ -1,2 +1,27 @@
 "use strict";
 // DOM Interaction & Typecasting
+// we add '!' for show typescript that we know exactly this element exist
+const anchor = document.querySelector('a');
+console.log(anchor.href);
+// const form = document.querySelector('form')!; // typescript says it's a HTMLFormElement
+// const form = document.querySelector('.new-item-form')!; // typescript says it's an Element
+/**
+ * when we use 'a' or 'form' tag, typescript knows what tag we grabbing
+ *
+ * in another case we use class and class could be apply to any element in the page
+ *
+ * we need to use typecasting:
+ */
+const form = document.querySelector('.new-item-form');
+// console.log(form.children);
+//inputs
+const type = document.querySelector('#type');
+const toFrom = document.querySelector('#tofrom');
+const details = document.querySelector('#details');
+const amounts = document.querySelector('#amount');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log(type.value, toFrom.value, details.value, 
+    // convert string to number, example:  '200' ->>  200
+    amounts.valueAsNumber);
+});
--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,42 @@
 // DOM Interaction & Typecasting
 
+// we add '!' for show typescript that we know exactly this element exist
+const anchor = document.querySelector('a')!;
+
+console.log(anchor.href);
+
+// const form = document.querySelector('form')!; // typescript says it's a HTMLFormElement
+// const form = document.querySelector('.new-item-form')!; // typescript says it's an Element
+/**
+ * when we use 'a' or 'form' tag, typescript knows what tag we grabbing
+ * 
+ * in another case we use class and class could be apply to any element in the page
+ * 
+ * we need to use typecasting:
+ */
+
+const form = document.querySelector('.new-item-form') as HTMLFormElement;
+
+// console.log(form.children);
+
+//inputs
+
+const type = document.querySelector('#type') as HTMLSelectElement;
+const toFrom = document.querySelector('#tofrom') as HTMLInputElement;
+const details = document.querySelector('#details') as HTMLInputElement;
+const amounts = document.querySelector('#amount') as HTMLInputElement;
+
+
+form.addEventListener('submit', (e: Event) => {
+  e.preventDefault();
+
+  console.log(
+    type.value,
+    toFrom.value,
+    details.value,
+    // convert string to number, example:  '200' ->>  200
+    amounts.valueAsNumber
+  );
+  
+});
+
