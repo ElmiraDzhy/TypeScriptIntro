9c06a25d08f60dc34ddfc1788e5eefbac6284b76
Elmira Dzhy <elmira.freshcode@gmail.com>
2023-09-05 16:50:10 +0300

    Create Invoice/Payment according to value

--- public/app.js
+++ public/app.js
@@ -1,23 +1,6 @@
 // Interfaces with Classes
 import Invoice from "./classes/Invoice.js";
 import Payment from "./classes/Payment.js";
-let docOne;
-let docTwo;
-docOne = new Invoice('yoshi', 'web work', 250);
-docTwo = new Payment('mario', 'plumbing work', 170);
-let docs = [];
-docs.push(docOne);
-docs.push(docTwo);
-console.log(docs);
-const inv = new Invoice('mario', 'work', 200);
-const inv2 = new Invoice('tom', 'work', 150);
-let invoices = [];
-invoices.push(inv);
-invoices.push(inv2);
-invoices.forEach(inv => {
-    console.log(inv.format());
-});
-//
 const form = document.querySelector('.new-item-form');
 //inputs
 const type = document.querySelector('#type');
@@ -26,7 +9,12 @@ const details = document.querySelector('#details');
 const amounts = document.querySelector('#amount');
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    console.log(type.value, toFrom.value, details.value, 
-    // convert string to number, example:  '200' ->>  200
-    amounts.valueAsNumber);
+    let doc;
+    if (type.value === 'invoice') {
+        doc = new Invoice(toFrom.value, details.value, amounts.valueAsNumber);
+    }
+    else {
+        doc = new Payment(toFrom.value, details.value, amounts.valueAsNumber);
+    }
+    console.log(doc);
 });
--- src/app.ts
+++ src/app.ts
@@ -4,33 +4,6 @@ import Invoice from "./classes/Invoice.js";
 import Payment from "./classes/Payment.js";
 import HasFormatter from "./intefaces/HasFormatter.js";
 
-let docOne: HasFormatter;
-let docTwo: HasFormatter;
-
-docOne = new Invoice('yoshi', 'web work', 250);
-docTwo = new Payment('mario', 'plumbing work', 170);
-
-let docs: HasFormatter[] = [];
-
-docs.push(docOne);
-docs.push(docTwo);
-
-console.log(docs);
-
-
-const inv = new Invoice('mario', 'work', 200);
-const inv2 = new Invoice('tom', 'work', 150);
-
-let invoices: Invoice[] = [];
-invoices.push(inv);
-invoices.push(inv2);
-
-invoices.forEach(inv => {
-  console.log(inv.format());
-})
-
-
-//
 
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
 
@@ -45,13 +18,16 @@ const amounts = document.querySelector('#amount') as HTMLInputElement;
 form.addEventListener('submit', (e: Event) => {
   e.preventDefault();
 
-  console.log(
-    type.value,
-    toFrom.value,
-    details.value,
-    // convert string to number, example:  '200' ->>  200
-    amounts.valueAsNumber
-  );
+  let doc: HasFormatter;
+
+  if (type.value === 'invoice') {
+    doc = new Invoice(toFrom.value, details.value, amounts.valueAsNumber);
+  }else{
+    doc = new Payment(toFrom.value, details.value, amounts.valueAsNumber);
+  }
+
+  console.log(doc);
+  
   
 });
 
