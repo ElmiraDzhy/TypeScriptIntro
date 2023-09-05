4155fd7917b7276683f2ab5e72358e08e31e2539
Elmira Dzhy <elmira.freshcode@gmail.com>
2023-09-05 13:15:26 +0300

    Add example of optional params in functions

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,2 +1,8 @@
 "use strict";
-console.log('test');
+let greet;
+greet = () => console.log('hello');
+// optional params :
+const add = (a, b, c) => {
+    console.log(a + b);
+};
+add(2, 3);
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,1 +1,11 @@
-console.log('test');
+let greet: Function;
+
+greet = () => console.log('hello');
+
+// optional params :
+const add = (a: number, b: number, c?: number|string) => {
+  console.log(a + b);
+  
+}
+
+add(2, 3);
