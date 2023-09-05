d6d12d29fb9a6c23f3bee0b07f2e9924c6902322
Elmira Dzhy <elmira.freshcode@gmail.com>
2023-09-05 13:24:56 +0300

    Add example of explicit value that function returns

--- public/sandbox.js
+++ public/sandbox.js
@@ -7,3 +7,8 @@ const add = (a, b, c = 10) => {
     console.log(c);
 };
 add(2, 3);
+const minus = (a, b) => {
+    return a - b;
+};
+let result = minus(10, 7);
+// result = 'string' ->> error
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -3,10 +3,18 @@ let greet: Function;
 greet = () => console.log('hello');
 
 // default value :
-const add = (a: number, b: number, c: number|string = 10) => {
+const add = (a: number, b: number, c: number|string = 10): void => {
   console.log(a + b);
   console.log(c);
-  
 }
 
 add(2, 3);
+
+const minus = (a: number, b: number): number => {
+  return a - b;
+}
+
+let result = minus(10, 7);
+
+// result = 'string' ->> error
+
